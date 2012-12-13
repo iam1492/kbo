214e31440a8eebbf1a0a74d1a0d60e008ac31d72
kai <lovefamilydev@gmail.com>
2012-12-13 23:46:46 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KboServer::Application.config.secret_token = '78b1ebc047697e719f60f85e19a72a4e351dc34a6bda3f75b9d83804eb91624eba57102bdfdcd4dd4e553f3999c4cd5c41af35f3c41a79cf2dcdb0274917301d'
