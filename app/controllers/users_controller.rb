cae1c2a1f42bb371f8a6fc0b169f38d88e31dcb4
Jung Kai <kai@kakao.com>
2012-12-31 18:52:41 +0900

    un block user 추가

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,24 @@ class UsersController < ApplicationController
   	@will_blocked_user = User.find_by_id(@will_blocked_user_id)
 
   	current_user.follow(@will_blocked_user)
-	meta_data = { :result => "success" }
+	  meta_data = { :result => "block user success" }
   	respond_to do |format|
     	format.html
     	format.json { render_for_api :user_without_comments, 
     		:json => @will_blocked_user, :root => :user, :meta => meta_data }
+    end
+  end
+
+  def un_block_user
+    @will_unblocked_user_id = params[:user_id]
+    @will_unblocked_user = User.find_by_id(@will_unblocked_user_id)
+
+    current_user.stop_following(@will_unblocked_user)
+    meta_data = { :result => "unblock user success" }
+    respond_to do |format|
+      format.html
+      format.json { render_for_api :user_without_comments, 
+        :json => @will_unblocked_user, :root => :user, :meta => meta_data }
     end
   end
 
